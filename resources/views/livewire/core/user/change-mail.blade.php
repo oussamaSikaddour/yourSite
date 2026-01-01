396606313dd8e7958ea0cc6a665751dbb523ed1f
oussama sikaddour <34127545+oussamaSikaddour@users.noreply.github.com>
2026-01-01 10:49:54 +0100

    push

--- resources/views/livewire/core/user/change-mail.blade.php
+++ resources/views/livewire/core/user/change-mail.blade.php
@@ -1,11 +1,11 @@
 <form class="form" wire:submit="handelSubmit" x-data="{ redirecting: false }"
     x-on:redirect-page.window="redirecting = true; setTimeout(() => { window.location.href = '{{ $this->logoutRoute() }}' }, 4500)">
     <h3>@lang('forms.change_mail.infos.redirect')</h3>
-    <div class="column">
+    <div class="column center">
         <x-core.form.input model="form.oldEmail" :label="__('forms.change_mail.mail')" type="email" html_id="FCPEmail" />
         <x-core.form.password-input :label="__('forms.change_mail.pwd')" model="form.password" html_id="CEPassword" />
     </div>
-    <div class="column">
+    <div class="column center">
         <x-core.form.input model="form.newEmail" :label="__('forms.change_mail.new_mail')" type="email" html_id="FCPNewEmail" />
     </div>
     <div class="form__actions">
--- resources/views/livewire/core/user/change-password.blade.php
+++ resources/views/livewire/core/user/change-password.blade.php
@@ -1,7 +1,7 @@
 <form class="form" wire:submit="handelSubmit" x-data="{ redirecting: false }"
     x-on:redirect-page.window="redirecting = true; setTimeout(() => { window.location.href = '{{ $this->logoutRoute() }}' }, 4500)">
     <h3>@lang('forms.change_password.infos.redirect')</h3>
-    <div class="column">
+    <div class="column center" >
         <x-core.form.password-input :label="__('forms.change_password.old_pwd')" model="form.password" html_id="CPPassword" />
         <x-core.form.password-input :label="__('forms.change_password.pwd')" model="form.newPassword" html_id="newPassword" />
     </div>
